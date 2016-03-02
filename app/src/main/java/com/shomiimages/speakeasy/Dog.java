9bcdf475fa09f05599d5c5af98b924031be0f4de
TheChientleGiant <thechientlegiant@gmail.com>
2016-03-02 12:03:31 -0800

    Have each class implement Speaker interface

--- app/src/main/java/com/shomiimages/speakeasy/Dog.java
+++ app/src/main/java/com/shomiimages/speakeasy/Dog.java
@@ -3,7 +3,7 @@ package com.shomiimages.speakeasy;
 /**
  * Created by Justin Dean Chien on 3/2/2016.
  */
-public class Dog {
+public class Dog implements Speaker{
     public String speak(){
         return "woof";
     }
